d351ac893bb161276c2d2b35e0454843506bdfd7
oliverdangl <oliverdangl@gmail.com>
2025-06-11 17:26:34 +0200

    changed port: 3000 added comments

--- app.js
+++ app.js
@@ -18,11 +18,12 @@ app.get('/', (req, res) => {
     res.redirect("/dashboard");
 });
 
-//Release 0: for rendering static site
+//Routing to /dashboard
 app.get("/dashboard", (req, res) => {
     res.render("dashboard");
 })
 
+//Routing to /users
 app.get("/users", (req, res) => {
     res.render("users");
 })
