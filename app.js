c2a995fd08c5face9c627fce88bc5b03098f7ff0
oliverdangl <oliverdangl@gmail.com>
2025-06-14 21:12:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const express = require("express");
+//Load environment variables from .env file
 const dotenv = require("dotenv");
-const pg = require("pg");//Access to postgres db through Node.js
-const bodyParser = require("body-parser");
-
-/* Reading global variables from config file */
 dotenv.config();
-const PORT = process.env.PORT;
 
+const express = require("express"); //Import express
+const pg = require("pg"); //Import PostgreSQL client library
+const bodyParser = require("body-parser"); //Import body-parser
+
+const app = express();
+
+
+//Read port number and database connection
+const PORT = process.env.PORT;
 const conString = process.env.DB_CON_STRING; //Includes parameters to connect
 
-//Forgot to set conString
-if(!conString) {
-    console.log("Error: environment variable DB_CON_STRING not set.");
-    process.exit(1);
-}
 
-//Configure connection and connect to client
+//Create a new Postgres client and connect
 const dbClient = new pg.Client({
     connectionString: conString,
     ssl: { rejectUnauthorized: false }
 });
+
 dbClient
     .connect()
     .then(() => console.log("Mit Datenbank verbunden"))
@@ -28,67 +28,71 @@ dbClient
         process.exit(1);
     });
 
-const urlencodedParser = bodyParser.urlencoded({
-    extended: false
-});
 
-
-app = express();
-
-//turn on serving static files (required for delivering css to client)
+//Serve files from "public"
 app.use(express.static("public"));
+//Parse URL-encoded bodies
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//Storing requested path
+//Store current request path in res.locals for template
 app.use((req,res,next) => {
     res.locals.currentPath = req.path;
     next();
 })
 
-//configure template engine
+//Tell Express where to find view template and that pug is used
 app.set("views", "views");
 app.set("view engine", "pug");
 
+//Redirect root URL to /dashboard
 app.get('/', (req, res) => {
     res.redirect("/dashboard");
 });
 
-//Routing to /dashboard
-app.get("/users", (req, res,next) => {
-    dbClient
-    .query(`
-        SELECT user_id, name, profile_pic
-        FROM users
-        ORDER BY name
-    `)
-    .then (({rows: users }) => {
-        res.render("users", {users});
-    })
-    .catch(next);
-})
 
-//Routing to /users
-app.get("/dashboard", (req, res,next) => {
+//Route: /dashboard
+//Get "others" posts with posts joined with users
+// => o.user_id as primary key and u.user_id as foreign key
+//After that render dashboard
+app.get("/dashboard", (req, res) => {
     dbClient
-    .query(`
-        SELECT o.post_id, o.text, o.created,
-                u.user_id, u.name, u.profile_pic
-        FROM others o
-        JOIN users u ON o.user_id = u.user_id
-        ORDER BY o.created DESC
-    `)
-    .then(({ rows: others }) => {
-        res.render("dashboard", {others})
-    })
-    .catch(next);
+        .query(`
+            SELECT o.post_id, o.text, o.created,
+                       u.user_id, u.name, u.profile_pic
+            FROM others o
+            JOIN users u ON o.user_id = u.user_id
+            ORDER BY o.created DESC
+        `)
+        .then(({ rows: others }) => {
+            res.render("dashboard", {others})
+        })
 });
 
+
+//Route: /users
+//Get all users (id, name, picture) sorted by name alphabetically
+//After that render users
+app.get("/users", (req, res) => {
+    dbClient
+        .query(`
+            SELECT user_id, name, profile_pic
+            FROM users
+            ORDER BY name
+        `)
+        .then (({rows: users }) => {
+             res.render("users", {users});
+        })
+})
+
+
 //Error handling
-app.use((err, req, res, next) =>{
+app.use((err, req, res) =>{
     console.log(err);
     res.status(500).send("Server Error");
 });
 
+
+//Listen on the configured port
 app.listen(PORT, function() {
   console.log(`OTHer running and listening on port ${PORT}`);
 });
